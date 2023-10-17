ba7f332aa48a962f24bcfc8ddefbd0a9835a4a99
AdityaPTN <adityasanuzie204@gmail.com>
2023-10-17 21:09:13 +0700

    add sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import { Sidebar, Navbar } from './components';
 const App = () => {
   return (
     <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
-      <div className='sm:flexx hidden mr-10 relative'>
+      <div className='sm:flex hidden mr-10 relative'>
         <Sidebar />
       </div>
 
